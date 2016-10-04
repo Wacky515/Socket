775a14f7fbdcaf7990b978ec337b7af17283377a
wacky515 <wacky515@gmail.com>
2016-10-05 07:42:08 +0900

    Update tcp

--- tcpclient.py
+++ tcpclient.py
@@ -14,7 +14,6 @@
 # モジュールインポート
 # from __future__ import print_function
 import os
-import sys
 import time
 import socket
 from contextlib import closing
@@ -23,10 +22,13 @@ from contextlib import closing
 class TcpCliCom:
     """ TCP/IP通信 """
     def __init__(self):
+        self.client_port = 9000
+        self.server_port = 60001
+
         if os.name == "nt":
             self.client_name = socket.gethostname()
             self.client_addr = (socket.gethostbyname(self.client_name),
-                            self.client_port)
+                                self.client_port)
             if self.client_name == "cad0021":
                 self.server_host = "172.21.38.192"
                 # self.server_host = "172.21.38.31"
@@ -34,18 +36,22 @@ class TcpCliCom:
             elif self.client_name == "PC-SA4110204580":
                 self.server_host = "192.168.1.5"
                 print("Selected Old Let's note")
+            elif self.client_name == "":
+                self.server_host = "192.168.1.5"
+                print("Selected New Let's note")
+            else:
+                self.server_host = "192.168.1.5"
+                print("In PXI")
 
-        elif sys.platform == "linux2":
+        elif os.name == "posix":
             self.client_name = os.uname()[1]
-            elif self.client_name == "ProSalad13.local":
+            if self.client_name == "ProSalad13.local":
                 self.server_host = "10.0.1.5"
                 print("Selected MacBook Pro")
             else:
-                self.server_host = client_addr
+                # TODO: "client_addr" を設定する
+                # self.server_host = client_addr
                 print("Selected unknouwn PC")
-
-        self.client_port = 9000
-        self.server_port = 60001
 
         print("Host name: " + self.client_name)
         print("Local addr: " + str(self.client_addr))
@@ -118,8 +124,6 @@ class TcpCliCom:
 
 def main():
     tcc = TcpCliCom()
-    print(tcc.client_name)
-    print(os.uname())
     # sended = tcc.send_client(host="192.168.1.5", port=60001, send_data="OK")
     # print("Send from python: " + str(sended))
 
